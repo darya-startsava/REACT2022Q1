b935af6bd7be1d92f2b1f6fa27a866994b8c806b
darya-startsava <daryastartsava@gmail.com>
2022-04-11 14:47:10 +0200

    fix: add missed route after merge commit

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import AboutUs from '../pages/AboutUs';
 import Error404 from '../pages/Error404';
+import Forms from '../pages/Forms';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="forms" element={<Forms />} />
       <Route path="about-us" element={<AboutUs />} />
       <Route path="*" element={<Error404 />} />
     </Routes>
