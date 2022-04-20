6f7b053d048318b6078f6d69d702a519116b3e23
darya-startsava <daryastartsava@gmail.com>
2022-04-20 10:40:55 +0200

    test: remove redundant snapshot tests

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -2,26 +2,55 @@ import Form from './Form';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-describe('form', () => {
-  it("should show error messages for empty fields, but shouldn't show them for completed fields(1-3)", () => {
-    const { container } = render(<Form />);
+const fakeData = [
+  { id: 1, name: '1' },
+  { id: 2, name: '2' },
+  { id: 3, name: '3' },
+];
 
-    userEvent.type(getName(), 'Name');
-    userEvent.click(getGender());
-    userEvent.type(getDate(), '2021-04-04');
-    userEvent.click(screen.getByRole('button', { name: /Submit/i }));
-    expect(container).toMatchSnapshot();
+describe('form', () => {
+  it('should show error messages for empty fields(4-6)', () => {
+    render(<Form />);
+
+    completeFields123();
+    expect(
+      screen.getByText('Choose country of birth') &&
+        screen.getByText('Choose at least one genre') &&
+        screen.getByText('Add image')
+    ).toBeInTheDocument();
   });
 
-  it("should show error messages for empty fields, but shouldn't show them for completed fields(4-6)", () => {
-    const { container } = render(<Form />);
-    const file = new File(['hello'], 'hello.png', { type: 'image/png' });
+  it("shouldn't show them for completed fields(1-3)", () => {
+    render(<Form />);
 
-    userEvent.selectOptions(getCountry(), ['UK']);
-    userEvent.click(getGenres());
-    userEvent.click(screen.getByRole('button', { name: /Submit/i }));
-    userEvent.upload(getImage(), file);
-    expect(container).toMatchSnapshot();
+    completeFields123();
+    expect(
+      screen.queryByText('This field requires more than 1 symbol') &&
+        screen.queryByText('This field must be filled') &&
+        screen.queryByText('Choose gender')
+    ).not.toBeInTheDocument();
+  });
+
+  it('should show error messages for empty fields(1-3)', () => {
+    render(<Form />);
+
+    completeFields456();
+    expect(
+      screen.getByText('This field requires more than 1 symbol') &&
+        screen.getByText('This field must be filled') &&
+        screen.getByText('Choose gender')
+    ).toBeInTheDocument();
+  });
+
+  it("shouldn't show them for completed fields(4-6)", () => {
+    render(<Form />);
+
+    completeFields456();
+    expect(
+      screen.queryByText('Choose country of birth') &&
+        screen.queryByText('Choose at least one genre') &&
+        screen.queryByText('Add image')
+    ).not.toBeInTheDocument();
   });
 
   it('should create a new card if all fields are filled', () => {
@@ -32,12 +61,12 @@ describe('form', () => {
   });
 
   it('should create 3 cards if submit 3 times', () => {
-    const { container } = render(<Form />);
+    render(<Form />);
 
     for (let i = 1; i <= 3; i++) {
       completeForm();
     }
-    expect(container).toMatchSnapshot();
+    expect(screen.getAllByRole('listitem').length).toEqual(fakeData.length);
   });
 });
 
@@ -69,6 +98,22 @@ function getGenres() {
 
 function getImage() {
   return screen.getByLabelText(/upload file/i);
+}
+
+function completeFields123() {
+  userEvent.type(getName(), 'Name');
+  userEvent.click(getGender());
+  userEvent.type(getDate(), '2021-04-04');
+  userEvent.click(screen.getByRole('button', { name: /Submit/i }));
+}
+
+function completeFields456() {
+  const file = new File(['hello'], 'hello.png', { type: 'image/png' });
+
+  userEvent.selectOptions(getCountry(), ['UK']);
+  userEvent.click(getGenres());
+  userEvent.upload(getImage(), file);
+  userEvent.click(screen.getByRole('button', { name: /Submit/i }));
 }
 
 function completeForm() {
