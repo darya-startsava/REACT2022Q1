b981928374a00359a6564a1975ce6b375659052a
darya-startsava <daryastartsava@gmail.com>
2022-04-25 15:46:15 +0200

    feat: add details to modal

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -15,18 +15,25 @@ export default function Card({
   dateOfBirth,
   countryOfBirth,
   movieGenres,
+  overview,
+  releaseDate,
+  voteAverage,
   isModal,
 }: CardType) {
+  let style = {};
+  if (isModal) {
+    style = { width: 500 };
+  }
   return (
-    <div className="my-1 card-item">
+    <div className="my-1 d-flex flex-column card-item" style={style}>
       {isModal && (
-        <button className="close-button" aria-label="close modal">
+        <button className="align-self-end close-button" aria-label="close modal">
           <img src="./svg/close.svg" className="close-img" alt="" />
         </button>
       )}
-      {image && <img src={image} className="card-image" alt="poster" />}
+      {image && <img src={image} className="align-self-center card-image" alt="poster" />}
       {uploadedImage && <img src={uploadedImage} className="card-image" alt="avatar" />}
-      <h5 className="card-title">{name}</h5>
+      <h5 className="align-self-center card-title">{name}</h5>
       {year && <p>{year} year</p>}
       {director && <p>Director: {director}</p>}
       {actors && <p>Starring: {actors ? actors.join(', ') : ''}</p>}
@@ -36,6 +43,9 @@ export default function Card({
       {dateOfBirth && <p>Date of birth: {dateOfBirth}</p>}
       {countryOfBirth && <p>Country of birth: {countryOfBirth}</p>}
       {movieGenres && <p>Genres: {movieGenres ? movieGenres.join(', ') : ''}</p>}
+      {isModal && <p>Overview: {overview}</p>}
+      {isModal && <p>Release date: {releaseDate}</p>}
+      {isModal && <p>Vote average: {voteAverage}</p>}
     </div>
   );
 }
