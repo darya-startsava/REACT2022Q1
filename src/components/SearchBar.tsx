82c7079b4aff6c3191edd1eb051fe1999c183591
darya-startsava <daryastartsava@gmail.com>
2022-05-03 10:05:14 +0200

    refactor: change serch bar into function component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,45 +1,36 @@
-import React, { ChangeEvent, KeyboardEvent } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useEffect, useState } from 'react';
 
 interface InputProps extends React.HTMLProps<HTMLInputElement> {
   onEnter?: Function;
 }
 
-export default class SearchBar extends React.Component<InputProps, { value: string }> {
-  constructor(props: InputProps) {
-    super(props);
-    this.state = {
-      value: localStorage.getItem('value') || '',
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
+export default function SearchBar(props: InputProps) {
+  const [value, setValue] = useState(localStorage.getItem('value') || '');
+
+  useEffect(() => {
+    return localStorage.setItem('value', value);
+  });
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
   }
 
-  componentWillUnmount() {
-    localStorage.setItem('value', this.state.value);
-  }
-
-  handleChange(event: ChangeEvent<HTMLInputElement>) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleKeyPress(event: KeyboardEvent<HTMLInputElement>) {
-    if (event.key === 'Enter' && this.props.onEnter) {
-      localStorage.setItem('value', this.state.value);
-      this.props.onEnter();
+  function handleKeyPress(event: KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter' && props.onEnter) {
+      localStorage.setItem('value', value);
+      props.onEnter();
     }
   }
 
-  render() {
-    return (
-      <label className="my-2">
-        <input
-          type="text"
-          placeholder="Search movie by title"
-          value={this.state.value}
-          onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress}
-        />
-      </label>
-    );
-  }
+  return (
+    <label className="my-2">
+      <input
+        type="text"
+        placeholder="Search movie by title"
+        value={value}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+      />
+    </label>
+  );
 }
